367472a247f1ae6b3036342e934dc65155b724a0
romit <11757603+romitjain@users.noreply.github.com>
2026-03-01 20:45:27 +0530

    v1 cuda kernel for gdn

--- solution/cuda/README.md
+++ solution/cuda/README.md
@@ -1,10 +1,11 @@
 # Kernel.cu
 
-Every kernel needs to read:
+We need to read:
 
 1. State (KxV)
 2. Key (1xK) and Value (1xV) vectors
 3. Query (1xK)
+4. Scalars (a, dt_bias, A_log, scale)
 
 and write:
 
@@ -15,4 +16,10 @@ and write:
 
 - Across number of tokens (B*T)
 - T == 1
-- Each block for one individual thread
+- Each block:
+  - Loads a single tile of state
+  - Updates a single tile of state
+  - Writes a single row of output
+- Design choices:
+  - Processing 2D tile of state
+  - Stride over K dimension inside the kernel
--- solution/cuda/kernel.cu
+++ solution/cuda/kernel.cu
@@ -7,12 +7,16 @@
  * See the track definition for required function signature and semantics.
  */
 
+//  Launch command
+// nvcc -std=c++17 -Wno-deprecated-gpu-targets -o kernel.o kernel.cu
+
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
 #include <mma.h>
 #include <cstddef>
 #include <cmath>
 #include <cstdio>
+#include <cmath>
 
 __device__ __forceinline__ float sigmoid(float x) {
     // Stable sigmoid
@@ -26,50 +30,116 @@ __device__ __forceinline__ float sigmoid(float x) {
 }
 
 __global__ void gdn_v1(
-    const void* q,
-    const void* k,
-    const void* v,
-    const float* state,
-    const float* A_log,
-    const float* a,
-    const float* dt_bias,
-    const float* b,
-    float* out,
-    float* new_state,
+    const float *__restrict__ q,
+    const float *__restrict__ k,
+    const float *__restrict__ v,
+    const float *__restrict__ state,
+    const float *__restrict__ A_log,
+    const float *__restrict__ a,
+    const float *__restrict__ dt_bias,
+    const float *__restrict__ b,
+    float *__restrict__ out,
+    float *__restrict__ new_state,
     int B,
     int num_v_heads,
+    int num_k_heads,
     int K,
     int V,
-    float scale
+    float scale,
+    int split_v
 ) {
-    // Read offsets for a, dt_bias, A_log, b
-    float gate = 0.0f;
-    float beta = 0.0f;
+    __shared__ float gate_s;
+    __shared__ float beta_s;
+    int common_scalar_offset = num_v_heads*blockIdx.x + blockIdx.y;
+    int qk_head_factor = num_v_heads/num_k_heads;
 
-    if (threadIdx.x % 32 == 0) {
-        int common_scalar_offset = B*num_v_heads*blockIdx.x + num_v_heads*blockIdx.y;
+    if ((threadIdx.x == 0) && (threadIdx.y == 0)) {
         float x = a[common_scalar_offset] + dt_bias[common_scalar_offset];
-        gate = expf(expf(A_log[common_scalar_offset]) * log1pf(expf(x)));
-        beta = sigmoid(b[common_scalar_offset]);
+        gate_s = expf(-expf(A_log[common_scalar_offset]) * log1pf(expf(x)));
+        beta_s = sigmoid(b[common_scalar_offset]);
+    }
+    __syncthreads();
+
+    float gate = gate_s;
+    float beta = beta_s;
+
+    __shared__ float STATE[8][32];
+    __shared__ float OLD_V[8]; // how to initialize to 0?
+    __shared__ float NEW_V[8];
+
+    int k_offset = (num_k_heads*blockIdx.x + blockIdx.y/qk_head_factor)*K;
+    int v_offset = common_scalar_offset*V;
+    // Get the read offset for state matrix (global)
+    // State matrix is BxHxVxK, we want VxK for every batch,head combination
+    int state_read_go = common_scalar_offset*V*K;
+    // Read offset for state matrix (local)
+    int local_v = blockIdx.z*split_v + threadIdx.y;
+    int state_read_lo = local_v*K;
+
+    float old_v_accum = 0.0f;
+
+    // Vector matmul b/w state and k
+    for (int k0=0;k0<K;k0+=32) {
+        // HMEM <> SMEM for state tile
+        int local_k = k0+threadIdx.x;
+        if (local_v<V and local_k<K) {
+            STATE[threadIdx.y][threadIdx.x] = state[state_read_go+state_read_lo+local_k];
+            old_v_accum += gate*STATE[threadIdx.y][threadIdx.x] * k[k_offset+local_k];
+        }
+        else {
+            STATE[threadIdx.y][threadIdx.x] = 0.0f;
+        }
     }
 
-    for (int offset = 16; offset > 0; offset /= 2)
-    {
-        gate = __shfl_xor_sync(0xffffffff, gate, offset, 32);
-        beta = __shfl_xor_sync(0xffffffff, beta, offset, 32);
+    // Warp reduction
+    for (int off = 16; off > 0; off >>= 1) {
+        old_v_accum += __shfl_down_sync(0xffffffff, old_v_accum, off);
+    }
+
+    // v_new computation
+    if (threadIdx.x == 0) {
+        OLD_V[threadIdx.y] = old_v_accum;
+        if (local_v < V) {
+            NEW_V[threadIdx.y] = beta * v[v_offset + local_v] + (1.0f - beta) * old_v_accum;
+        } else {
+            NEW_V[threadIdx.y] = 0.0f;
+        }
     }
 
     __syncthreads();
 
-    // Get the correct read offset for state matrix
-    int state_read_in = B*num_v_heads*blockIdx.x + num_v_heads*blockIdx.y;
+    float out_acc = 0.0f;
 
+    // Updated state computation and write back
+    for (int k0=0; k0<K ;k0+=32) {
+        int local_k = k0+threadIdx.x;
+        if (local_v < V and local_k < K) {
+            float s = state[state_read_go+state_read_lo+local_k];
+            float kval = k[k_offset+local_k];
+            float val = gate*s - OLD_V[threadIdx.y] * kval + NEW_V[threadIdx.y] * kval;
+
+            new_state[state_read_go+state_read_lo+local_k] = val;
+
+            out_acc += scale * val * q[k_offset+local_k];
+        }
+    }
+
+    for (int off = 16; off > 0; off >>= 1) {
+        out_acc += __shfl_down_sync(0xffffffff, out_acc, off);
+    }
+
+    // Write out
+    if (threadIdx.x == 0) {
+        if (local_v < V) {
+            out[v_offset+local_v] = out_acc;
+        }
+    }
 }
 
 extern "C" cudaError_t launch_gdn_v1(
-    const void* q,
-    const void* k,
-    const void* v,
+    const float* q,
+    const float* k,
+    const float* v,
     const float* state,
     const float* A_log,
     const float* a,
@@ -79,14 +149,17 @@ extern "C" cudaError_t launch_gdn_v1(
     float* new_state,
     int B,
     int num_v_heads,
+    int num_k_heads,
     int K,
     int V,
     float scale,
     cudaStream_t stream
 ) {
 
-    dim3 threads_per_block(K, 16);
-    dim3 grid_size(B, num_v_heads);
+    // V dim is split into these many blocks
+    int split_v = 8;
+    dim3 threads_per_block(32, split_v);
+    dim3 grid_size(B, num_v_heads, (V+split_v-1)/split_v);
 
     gdn_v1<<<grid_size, threads_per_block>>>(
         q,
@@ -101,9 +174,11 @@ extern "C" cudaError_t launch_gdn_v1(
         new_state,
         B,
         num_v_heads,
+        num_k_heads,
         K,
         V,
-        scale
+        scale,
+        split_v
     );
     return cudaGetLastError();
 }
@@ -125,9 +200,9 @@ int main() {
     const size_t gate_elems = B * num_v_heads;
     const size_t out_elems = B * num_v_heads * V;
 
-    half* q = nullptr;
-    half* k = nullptr;
-    half* v = nullptr;
+    float* q = nullptr;
+    float* k = nullptr;
+    float* v = nullptr;
     float* state = nullptr;
     float* A_log = nullptr;
     float* a = nullptr;
@@ -137,9 +212,9 @@ int main() {
     float* new_state = nullptr;
     const float scale = 1.0f / std::sqrt(static_cast<float>(K));
 
-    cudaMallocManaged(&q, q_elems * sizeof(half));
-    cudaMallocManaged(&k, k_elems * sizeof(half));
-    cudaMallocManaged(&v, v_elems * sizeof(half));
+    cudaMallocManaged(&q, q_elems * sizeof(float));
+    cudaMallocManaged(&k, k_elems * sizeof(float));
+    cudaMallocManaged(&v, v_elems * sizeof(float));
     cudaMallocManaged(&state, state_elems * sizeof(float));
     cudaMallocManaged(&A_log, gate_elems * sizeof(float));
     cudaMallocManaged(&a, gate_elems * sizeof(float));
@@ -148,8 +223,8 @@ int main() {
     cudaMallocManaged(&out, out_elems * sizeof(float));
     cudaMallocManaged(&new_state, state_elems * sizeof(float));
 
-    for (size_t i = 0; i < k_elems; ++i) k[i] = __float2half(0.02f * static_cast<float>(i % 13));
-    for (size_t i = 0; i < v_elems; ++i) v[i] = __float2half(0.03f * static_cast<float>(i % 11));
+    for (size_t i = 0; i < k_elems; ++i) k[i] = (0.02f * static_cast<float>(i % 13));
+    for (size_t i = 0; i < v_elems; ++i) v[i] = (0.03f * static_cast<float>(i % 11));
     for (size_t i = 0; i < state_elems; ++i) {
         state[i] = 0.0f;
         new_state[i] = 0.0f;
@@ -174,6 +249,7 @@ int main() {
         out,
         new_state,
         B,
+        num_v_heads,
         num_k_heads,
         K,
         V,
@@ -244,6 +320,7 @@ void launch_gdn_v1_torch(
         out.data_ptr<float>(),
         new_state.data_ptr<float>(),
         B,
+        num_v_heads,
         num_k_heads,
         K,
         V,
